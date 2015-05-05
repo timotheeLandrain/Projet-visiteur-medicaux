418dfa612275ec015814dffe523a6fa032ba1a42
timotheeLandrain <sio.landrain.timothee@gmail.com>
2015-05-05 09:53:18 +0200

    amélioration de la page ajouter un visiteur

--- code source/appliFrais/AjouterUnVisiteur.php
+++ code source/appliFrais/AjouterUnVisiteur.php
@@ -24,9 +24,36 @@
             <legend>Données du visiteur à entrer
             </legend>
 			<table>
-			
+			<p>
 			<label for="nom">Nom : </label>
               <input type="text" id="nom" name="nom" size="12" maxlength="10" 
                      title="Entrez le nom du visiteur"/>
+			</p>
+			<p>
+			<label for="prenom">Prenom :</label>
+			  <input type="text" id="prenom" name="prenom" size="12" maxlength="10" 
+                     title="Entrez le prenom du visiteur"/>
+			</p>
+			<p>
+			<label for="codepostale">Code Postal :</label>
+			  <input type="text" id="codepostale" name="codepostale" size="12" maxlength="10" 
+                     title="Entrez le codepostale du visiteur"/>
+			</p>
+			<p>
+			<label for="ville">Ville :</label>
+			  <input type="text" id="ville" name="ville" size="12" maxlength="10" 
+                     title="Entrez la ville du visiteur"/>
+			</p>
+			<p>
+			<label for="codepostale">Adresse :</label>
+			  <input type="text" id="adresse" name="adresse" size="12" maxlength="10" 
+                     title="Entrez l'adresse du visiteur"/>
+			</p>
+			<p>
+			<label for="DateEmbauche">Date d'embauche :</label>
+			  <input type="text" id="dateembauche" name="dateembauche" size="12" maxlength="10" 
+                     title="Entrez la date d'embauche du visiteur"/>
+			</p>
+			<p>
 			<INPUT type="submit" value="Valider"/>
-			
+			</p>
