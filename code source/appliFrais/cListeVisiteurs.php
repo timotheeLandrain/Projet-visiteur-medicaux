37dab86d9c4c48ac07a5bd987a579bbfb18da63f
bveslin <sio.veslin.benjamin@gmail.com>
2015-09-09 16:24:24 +0200

    Tri cListeVisiteurs
    Tri par ville

--- code source/appliFrais/cListeVisiteurs.php
+++ code source/appliFrais/cListeVisiteurs.php
@@ -14,59 +14,137 @@
   }
   require($repInclude . "_entete.inc.html");
   require($repInclude . "_sommaire.inc.php");
+  
+  $liste = 'SELECT * FROM visiteur';
+  $reqliste = mysqli_query(connecterServeurBD(), $liste) or die('Erreur SQL !<br />'.$liste.'<br />'.mysqli_error());
 ?>
   <!-- Division principale -->
   <div id="contenu">
 		<h2>Liste visiteurs</h2>
-		<?php
-		$liste = 'SELECT * FROM visiteur';
-		$reqliste = mysqli_query(connecterServeurBD(), $liste) or die('Erreur SQL !<br />'.$liste.'<br />'.mysqli_error());
-		?>
 		<FORM action='cListeVisiteurs.php' method=POST>
-		<table align=center, border="1">
-			<td>
-				<b>Nom</b>
-			</td>
-			<td>
-				<b>Prenom</b>
-			</td>
-			<td>
-				<b>Ville</b>
-			</td>
-			<td>
-				<b>Cabinet</b>
-			</td>
+			<SELECT name="Ville_Choix">
+				<OPTION value="%"> Ville
+				<?php
+					$listeVille = "SELECT distinct ville FROM visiteur";
+					$reqListeVille = mysqli_query(connecterServeurBD(), $listeVille);
+					while($take = mysqli_fetch_array($reqListeVille)){
+						?><OPTION><?php echo $take['ville'];
+					}
+				?>
+				<br>
+			</SELECT>
+		<INPUT type="submit" name="OK" value="OK"/>
+		</FORM>
 		<?php
-		while ($data = mysqli_fetch_array($reqliste)) {
+		if(isset($_POST['OK'])){
 		?>
-			<tr>
+			<FORM action='cListeVisiteurs.php' method=POST>
+			<?php
+			$liste = 'SELECT * FROM visiteur WHERE ville like "'.$_POST['Ville_Choix'].'"';
+			$reqliste = mysqli_query(connecterServeurBD(), $liste) or die('Erreur SQL !<br />'.$liste.'<br />'.mysqli_error());
+			?>
+			<table align=center, border="1">
 				<td>
-				<?php
-					echo $data['nom'].'<br />';
-				?>
+					<b>Nom</b>
 				</td>
 				<td>
-				<?php
-					echo $data['prenom'].'<br />';
-				?>
+					<b>Prenom</b>
 				</td>
 				<td>
-				<?php
-					echo $data['ville'].'<br />';
-				?>
+					<b>Ville</b>
 				</td>
 				<td>
-				<?php
-					echo 'None<br />';
-				?>
+					<b>Cabinet</b>
 				</td>
-			</tr>
+			<?php
+			while ($data = mysqli_fetch_array($reqliste)) {
+			?>
+				<tr>
+					<td>
+					<?php
+						echo $data['nom'].'<br />';
+					?>
+					</td>
+					<td>
+					<?php
+						echo $data['prenom'].'<br />';
+					?>
+					</td>
+					<td>
+					<?php
+						echo $data['ville'].'<br />';
+					?>
+					</td>
+					<td>
+					<?php
+						echo 'None<br />';
+					?>
+					</td>
+				</tr>
 
 
+			<?php
+			}		
+			?>
+			</table>
+			</FORM>
 		<?php
-		}		
+		}
+		else{
 		?>
-		</table>
+			<FORM action='cListeVisiteurs.php' method=POST>
+			<?php
+			$liste = 'SELECT * FROM visiteur';
+			$reqliste = mysqli_query(connecterServeurBD(), $liste) or die('Erreur SQL !<br />'.$liste.'<br />'.mysqli_error());
+			?>
+			<table align=center, border="1">
+				<td>
+					<b>Nom</b>
+				</td>
+				<td>
+					<b>Prenom</b>
+				</td>
+				<td>
+					<b>Ville</b>
+				</td>
+				<td>
+					<b>Cabinet</b>
+				</td>
+			<?php
+			while ($data = mysqli_fetch_array($reqliste)) {
+			?>
+				<tr>
+					<td>
+					<?php
+						echo $data['nom'].'<br />';
+					?>
+					</td>
+					<td>
+					<?php
+						echo $data['prenom'].'<br />';
+					?>
+					</td>
+					<td>
+					<?php
+						echo $data['ville'].'<br />';
+					?>
+					</td>
+					<td>
+					<?php
+						echo 'None<br />';
+					?>
+					</td>
+				</tr>
+
+
+			<?php
+			}		
+			?>
+			</table>
+			</FORM>
+		<?php
+		}
+		?>
 
   </div>
   
